b9e826a1398f79adb922c848282827464dac30cc
unknown <swetazalavadiya9@gmail.com>
2022-10-21 01:12:05 +0530

    votingid

--- src/routes/route.js
+++ src/routes/route.js
@@ -89,43 +89,43 @@ let players =
       }
    })
 
-//    let persons= [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-//  },
-//  {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-//  },
-//  {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-//  },
-//  {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-//  },
-//  {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-//  }
-//  ]
-//    router.post('/person',function(req,res){
-//     const votingAge=req.query.votingAge
+   let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+   router.post('/person',function(req,res){
+    const votingAge=req.query.votingAge
 
-//     const updatedPersons =[]
-//     persons.forEach((person) => {
-//         if(person.age>votingAge){
-//             person.votingStatus=true
-//             updatedPersons.push(person)
-//         }
-//     });
-//     return res.send({updatedPersons:updatedPersons})
-// })
+    const updatedPersons =[]
+    persons.forEach((person) => {
+        if(person.age>votingAge){
+            person.votingStatus=true
+            updatedPersons.push(person)
+        }
+    });
+    return res.send({updatedPersons:updatedPersons})
+})
 module.exports = router;
