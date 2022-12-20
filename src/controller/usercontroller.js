8ea3fd1b1c3c26f1fa06d4cfeb7a66d730b750de
1siikaa <srishtidwivedi672@gmail.com>
2022-12-20 10:08:17 +0530

    done

--- src/controller/usercontroller.js
+++ src/controller/usercontroller.js
@@ -1,6 +1,7 @@
 const { isValidName, forName, isValidEmail, isValidNumber, isValidPassword, isValidObjectId, isValidPincode }= require('../validator/validation.js')
 const userModel= require('../models/usermodel.js')
 const bcrypt= require('bcrypt')
+const jwt= require('jsonwebtoken')
 
 
 const registerUser = async function(req, res){
